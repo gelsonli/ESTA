f9c9a881523745f73087ac8f87dc759d54252b58
Gelson Lima de Souza Junior <153616550+gelsonli@users.noreply.github.com>
2024-08-09 11:52:21 -0300

    css

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
 (function () {
-    
-    let name = "cde"
+    const $ = q => document.querySelector(q);
+
+    function addCarToGarage (car) {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>${car.name}</td>
+            <td>${car.licence}</td>
+            <td>${car.time}</td>
+            <td>
+                <botom class="delete"></botom>
+            </td>
+        `
+        $("#garage").appendChild(row);
+    };
+
+    $("send").addEventListenner("click", e => {
+        const name = $("#name").value
+        const licence = $("#licence").value;
+
+        if(!name || !licence) {
+            alert("Os campos são obrigatórios!");
+            return;
+        }
+
+        const car = { nome, licence, time: new Date() }
+
+        const garage = localStorage.garage ? JSON.parse(localStorage.garage) : [];
+        garage.push(car);
+
+        localStorage.garage = JSON.stringify(garage);
+        console.log(garage);
+
+        addCarToGarage(car);
+
+        $("#name").value = "";
+        $("#licence").value = "";
+    });
 })();
 
